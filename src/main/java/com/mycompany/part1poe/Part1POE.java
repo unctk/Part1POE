0fde4705050611e7b9a823a399a963aaeb507c12
unctk <ST10511101@rcconnect.edu.za>
2026-09-17 21:15:16 +0200

    Part 1
    Added the declarations and asked the user to enter first and last name

--- src/main/java/com/mycompany/part1poe/Part1POE.java
+++ src/main/java/com/mycompany/part1poe/Part1POE.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.part1poe;
 
+import java.util.Scanner;
 /**
  *
  * @author Student
@@ -11,6 +12,25 @@ package com.mycompany.part1poe;
 public class Part1POE {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //Declarations
+       String firstName;
+       String lastName;
+       String username;
+       String password;
+       String cellNumber;
+
+             
+      //Scanner input = new Scanner (); 
+       Scanner input = new Scanner(System.in);
+       Login login = new Login();
+       
+       
+       // Ask for users first name and last name
+       System.out.print("Please enter your first name: ");
+       firstName = input.nextLine();
+       
+       System.out.print("Please enter your last name: ");
+       lastName = input.nextLine();
+       
     }
 }
