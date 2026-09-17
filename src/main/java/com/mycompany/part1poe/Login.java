f80d2e58a2a77fb6130351ab8a922f7c5adde6e7
unctk <ST10511101@rcconnect.edu.za>
2026-09-17 22:50:29 +0200

    login class, added regex pattern, declaration and 2 methods
    Regex pattern for number formatting
    
    methods:
    checkUserName and checkCellPhoneNumber

--- src/main/java/com/mycompany/part1poe/Login.java
+++ src/main/java/com/mycompany/part1poe/Login.java
@@ -3,11 +3,51 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.part1poe;
-
+import java.util.regex.Pattern;
+//For the formatting of the cell phone number
 /**
  *
  * @author Student
  */
 public class Login {
     
+    //declaration
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String cellNumber;
+    private boolean registered = false; 
+    
+    
+     // method checks whether username contains "_" and is <=5      
+    public static boolean checkUserName(String username) {
+        if (username == null) {
+            return false;
+        }
+        if (username.contains("_") && username.length() <= 5) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }        
+     
+    
+    //checks if method has correct international code and if numbers are not too many
+    public static boolean checkCellPhoneNumber(String cellNumber) {
+        String pattern = "\\+27[0-9]{1,10}";
+        
+        if (cellNumber == null || cellNumber.isEmpty()) {
+            return false;
+        }
+        if (Pattern.matches(pattern, cellNumber)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }        
+            
+            
 }
