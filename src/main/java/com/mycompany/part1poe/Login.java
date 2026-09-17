dbcccae0e4269be0eea904beaf69db0324e948f3
unctk <ST10511101@rcconnect.edu.za>
2026-09-17 23:17:16 +0200

    added proper comments

--- src/main/java/com/mycompany/part1poe/Login.java
+++ src/main/java/com/mycompany/part1poe/Login.java
@@ -4,14 +4,14 @@
  */
 package com.mycompany.part1poe;
 import java.util.regex.Pattern;
-//For the formatting of the cell phone number
 /**
- *
+ *handles user reg and log in validation
+ * 
  * @author Student
  */
 public class Login {
     
-    //declaration
+    //declarations are made not static meaning every new login gets its own copy separetly
     private String firstName;
     private String lastName;
     private String username;
@@ -34,9 +34,9 @@ public class Login {
     }        
      
     
-    //checks if method has correct international code and if numbers are not too many
+    //checks if method has correct international code(+27) and digits of 1 to 10
     public static boolean checkCellPhoneNumber(String cellNumber) {
-        String pattern = "\\+27[0-9]{1,10}";
+        String pattern = "\\+27[0-9]{1,10}";   // \\+27 a literal and [0-9] is any single digit and {1,10} between 1 & 10 of those digits
         
         if (cellNumber == null || cellNumber.isEmpty()) {
             return false;
@@ -48,6 +48,33 @@ public class Login {
             return false;
         }
     }        
-            
+      // checks if password is at least 8 char long and has capital letter, number ans special char
+    public static boolean checkPasswordComplexity(String password) {
+        if (password == null || password.length() <8 ) {
+            return false;
+        }
+               boolean hasCapital = false;
+               boolean hasSpecial = false;      
+               boolean hasNumber = false;
+               
+               for(int i = 0; i < password.length(); i++){
+                   char ch = password.charAt(i);
+                   
+               if(Character.isDigit(ch)){
+                  hasNumber = true; 
+                  }
+               else if (Character.isUpperCase(ch)) {
+                   hasCapital = true;
+                  }
+               // anything that is not a letter or not a degit counts a special character !, @
+               else if (!Character.isLetterOrDigit(ch)) {
+                   hasSpecial = true;
+               }
+               
+             }
+            //password passes only if all 3 conditions are met within string   
+        return hasCapital && hasNumber && hasSpecial;
+        
+    }
             
 }
