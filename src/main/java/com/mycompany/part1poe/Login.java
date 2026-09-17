91cef27a93f42440bd6712fbd0ae70054f7aecff
unctk <ST10511101@rcconnect.edu.za>
2026-09-17 23:47:37 +0200

    registerUser ,loginUser,returnLoginStatus, etc.

--- src/main/java/com/mycompany/part1poe/Login.java
+++ src/main/java/com/mycompany/part1poe/Login.java
@@ -74,7 +74,53 @@ public class Login {
              }
             //password passes only if all 3 conditions are met within string   
         return hasCapital && hasNumber && hasSpecial;
+         }
+    
+    //validates all 3 fields and if all is valid
+          public String registerUser(String firstName, String lastName, String username, String password, String cellNumber) {
+        
+        if(!checkUserName(username)){
+            return "Username is not correctly formatted; please "+ "ensure that your username contains an underscore and "+ "is no more than five characters in length.";
+        }
+        if (!checkPasswordComplexity(password)) {
+            return "Password is not correctly formatted; please " + "ensure that the password contains at least eight characters, " + " a capital letter, a number, and a special character.";
+        }  
+        if(!checkCellPhoneNumber(cellNumber)) {
+            return "Cell number is incorrectly formatted or does not contain an international code; please correct the number and try again.";
+        }
+        //3 checks passed, saves the details against object so loginUser can verify later
+       this.firstName = firstName; 
+       this.lastName = lastName;
+       this.username = username;
+       this.password = password;
+       this.cellNumber = cellNumber;
+       this.registered = true;
+       
+       return "Username successfully captured.\n" + "Password successfully captured.\n" + "Cell number successfully captured.\n" + "Registration successful!";
+          }
+          
+          //verifies if entered username and password match from when registerUser succceede
+          public boolean loginUser (String enteredUsername, String enteredPassword) {
+       if (!registered) {
+           return false;
+       }
+       return this.username.equals(enteredUsername) && this.password.equals(enteredPassword);
+   } 
+    public String returnLoginStatus(boolean loginSuccess){
+        if (loginSuccess) {
+            return "Welcome " + firstName + ", " + lastName + " it is great to see you again.";
+        }
+        else {
+            return "Username or password incorrect, please try again.";
+        }
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+        
         
     }
-            
 }
+
