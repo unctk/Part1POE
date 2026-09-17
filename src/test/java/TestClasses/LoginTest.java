ed09f2bf9e99469baa26c06b8915442d1d86171a
unctk <ST10511101@rcconnect.edu.za>
2026-09-18 00:02:47 +0200

    added Junit class

---
+++ src/test/java/TestClasses/LoginTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package TestClasses;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Student
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+    
+}
